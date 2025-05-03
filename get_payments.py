9bc6900e5fe1b97e24c5d6407403a35b7a3ce173
AukeSonneveld <auke.sonneveld@gmail.com>
2025-05-03 12:15:41 +0200

    whole bunch of stuff: database for payment summeries, dummy values, etc.

--- get_payments.py
+++ get_payments.py
@@ -1,17 +1,13 @@
 from bunq.sdk.context.api_context import ApiContext
 from bunq.sdk.context.bunq_context import BunqContext
 from bunq import ApiEnvironmentType
-from bunq.sdk.model.generated.endpoint import MonetaryAccountBankApiObject, PaymentApiObject
+from bunq.sdk.model.generated.endpoint import MonetaryAccountBankApiObject, PaymentApiObject, EventApiObject
 from bunq import Pagination
 from json_converters import payment_to_json
+import json
 import analyze_payments
-
-API_ENVIRONMENT = ApiEnvironmentType.SANDBOX
-API_KEYS = ["sandbox_c9b52ea14aaa43bf09d656cfffad510beea3a32a584e9529571cc494",
-            "d527043b64f42899fbd7ae29c218236fc6ce0859af3006d1b4946def64092150",
-            "a2a0bae07854e6e7514c027bb5f9f2a5456f93df6e3a6540ce569204b4343e43"]
-API_KEY = API_KEYS[2]
-DEVICE_DESCRIPTION = "My Device Description"
+from database import PaymentDatabase
+from config import API_ENVIRONMENT, API_KEY, DEVICE_DESCRIPTION
 
 def setup_api_context():
     """
@@ -56,24 +52,28 @@ def get_user_payments(user_id, monetary_account_id, count=10):
     
     return payments
 
-if __name__ == "__main__":
-
-    user_context, user_id = setup_api_context()
-
-    # Get monetary account ID that has a balance > 0
-    ma_list = MonetaryAccountBankApiObject.list().value
-    monetary_account_id = None
-    for ma in ma_list:
-        if float(ma.balance.value) > 0:
-            monetary_account_id = ma.id_
-            break
-
-    # Get payments
-    if monetary_account_id:
-        payments = get_user_payments(user_id, monetary_account_id)
+def get_user_events(monetary_account_ids, count=10):
+    """
+    Get events for a specified user.
     
-    payment_jsons = [payment_to_json(payment) for payment in payments]
-    summaries = analyze_payments.analyze_payments(payment_jsons, analyze_payments.prompt_no_examples)
+    Args:
+        monetary_account_ids: Set of monetary account IDs
+        count: Number of events to retrieve (default: 10)
     
-
+    Returns:
+        List of event objects
+    """
+    # Create pagination 
+    pagination = Pagination()
+    pagination.count = count
+    
+    # List events
+    events = EventApiObject.list(
+        params={
+            'monetary_account_id': ','.join(map(str, monetary_account_ids)),
+            'display_user_event': 'false'
+        }
+    ).value
+    
+    return events
 
