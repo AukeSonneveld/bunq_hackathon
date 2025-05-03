6d31556600d8cc299e5654e789de912695775003
Steven van den Wildenberg <stevenvandenwildenberg@gmail.com>
2025-05-03 04:03:03 +0200

    Setup simple function calling

--- steven/client.py
+++ steven/client.py
@@ -40,41 +40,60 @@ async def run():
             read,
             write,
         ) as session:
-            prompt = "Hey how is your session going?"
-            # Initialize the connection
+            prompt = "Please check my bunq bank account balance and greet with my name Steven"
             await session.initialize()
-            # Get tools from MCP session and convert to Gemini Tool objects
-            mcp_tools = await session.list_resource_templates()
-            tools = types.Tool(function_declarations=[
+
+            mcp_resource_temp = await session.list_resource_templates()
+            resourceTemplates = [
                 {
                     "name": tool.name,
                     "description": tool.description,
                     "parameters": uri_template_to_json_schema(tool.uriTemplate),
                 }
-                for tool in mcp_tools.resourceTemplates
-            ])
-            
-            # Send request with function declarations
+                for tool in mcp_resource_temp.resourceTemplates
+            ]
+            mcp_resources = await session.list_resources()
+            resources = [
+                {
+                    "name": tool.name.replace("://", "_").replace("/", "_"),
+                    "description": tool.description,
+                    "parameters": None,
+                }
+                for tool in mcp_resources.resources
+            ]
+            tools = types.Tool(function_declarations=resources + resourceTemplates)
+
+
             response = client.models.generate_content(
-                model="gemini-2.0-flash",  # Or your preferred model supporting function calling
+                model="gemini-2.5-flash-preview-04-17",
                 contents=prompt,
                 config=types.GenerateContentConfig(
-                    temperature=0.7,
+                    temperature=0.2,
                     tools=[tools],
-                ),  # Example other config
+                ),
             )
-            # Check for a function call
-            if response.candidates[0].content.parts[0].function_call:
-                function_call = response.candidates[0].content.parts[0].function_call
-                print(f"Function to call: {function_call.name}")
-                print(f"Arguments: {function_call.args}")
+           
+            for idx, func_call in enumerate(response.candidates[0].content.parts):
+                print(f"Possible function call: {idx}")
+                if func_call.function_call:
+                    function_call = response.candidates[0].content.parts[0].function_call
+                    print(f"Function to call: {function_call.name}")
+                    print(f"Arguments: {function_call.args}")
 
-                template = next(rt for rt in mcp_tools.resourceTemplates if rt.name == function_call.name)
-                uri = fill_uri_template(template.uriTemplate, function_call.args)
-                result = await session.read_resource(uri)
-                print(f"Function result: {result}")
-
-            else:
+                    if function_call.name == "get_greeting":
+                        template = next(rt for rt in mcp_resource_temp.resourceTemplates if rt.name == function_call.name)
+                        uri = fill_uri_template(template.uriTemplate, function_call.args)
+                        result = await session.read_resource(uri)
+                        print(f"Function result: {result}")
+                    else:
+                        result = await session.read_resource("bunq://user/account/primaryMonetary/balance")
+                        print(f"Function result: {result}")
+                        print(response.text)
+                else:
+                    print("No function call found in the response.")
+                    print(response.text)
+            
+            if not response.candidates[0].content.parts[0].function_call:
                 print("No function call found in the response.")
                 print(response.text)
             
--- steven/server.py
+++ steven/server.py
@@ -1,4 +1,22 @@
 from mcp.server.fastmcp import FastMCP
+from bunq.sdk.context.api_context import ApiContext
+from bunq.sdk.context.bunq_context import BunqContext
+from bunq import ApiEnvironmentType
+from bunq.sdk.model.generated.endpoint import MonetaryAccountBankApiObject
+
+API_KEYS = ["sandbox_c9b52ea14aaa43bf09d656cfffad510beea3a32a584e9529571cc494", # Self generated
+            "d527043b64f42899fbd7ae29c218236fc6ce0859af3006d1b4946def64092150",
+            "a2a0bae07854e6e7514c027bb5f9f2a5456f93df6e3a6540ce569204b4343e43"]
+
+api_context = ApiContext.create(
+    ApiEnvironmentType.SANDBOX,
+    API_KEYS[1],
+    "My Device Description"
+)
+BunqContext.load_api_context(api_context)
+user_context = BunqContext.user_context()
+
+user_id = user_context.user_id
 
 mcp = FastMCP("Bunq API Server")
 
@@ -6,6 +24,12 @@ mcp = FastMCP("Bunq API Server")
 def get_greeting(name: str) -> str:
     """Get a personalized greeting"""
     return f"Hello, we're at the BUNQ hackathon, {name}!"
+
+@mcp.resource("bunq://user/account/primaryMonetary/balance")
+def get_balance() -> str:
+    """Get the current bank account balance of the primary account of the user"""
+    primary_account: MonetaryAccountBankApiObject = user_context.primary_monetary_account
+    return primary_account.balance.value
 
 if __name__ == "__main__":
     mcp.run()
